ae6aa6c7d3bcd85cbce71cbe4d56d47182e876ee
ArjunBollam <bollamarjun1@gmail.com>
2018-03-21 16:13:45 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/home.component';
-import { PortfolioComponent } from './Portfolio/portfolio.component';
 import { PageService } from './page.service';
 import { MovieListComponent } from './Movies/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, PortfolioComponent, MovieListComponent
+    AppComponent, HomeComponent, MovieListComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot([
-      {path : 'portfolio', component: PortfolioComponent },
       {path: 'home', component: HomeComponent},
       {path: '', redirectTo: 'home', pathMatch: 'full'}
     ])
